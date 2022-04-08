25afac284ed6a3902a2316b00bd3b124f981caba
Greg611 <varnida@gmail.com>
2022-04-08 21:44:16 +0200

    *

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,12 @@
 public class Main {
 
+    public static void main(String[] args) {
+        System.out.print( "Válassza ki mit szeretne tenni:\n" +
+                            "\t1- Listázás\n" +
+                            "\t2- Új elem\n" +
+                            "\t3- Elem módosítása\n" +
+                            "\t4- Elem törlése\n" +
+                            "\t0- Kilépés");
+    }
 
 }
